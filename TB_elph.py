d606ad343e601bd229f2ba0c4cc742a88717752b
Danis Badrtdinov <danis@n022173.science.ru.nl>
2024-04-09 17:36:05 +0200

    lambda constant implementation

--- TB_elph.py
+++ TB_elph.py
@@ -15,16 +15,15 @@ def kmesh_preparation(kmesh, cell_vec):
     rec_vec[1]  = (2 * np.pi / np.linalg.det(cell_vec)) * np.cross(cell_vec[2], cell_vec[0])
     rec_vec[2]  = (2 * np.pi / np.linalg.det(cell_vec)) * np.cross(cell_vec[0], cell_vec[1])
 
-    for q1 in range(kmesh[0]):
-        for q2 in range(kmesh[1]):
-            for q3 in range(kmesh[2]):
-                q = (q1 * kmesh[1] * kmesh[2]) + (q2 * kmesh[2]) + q3
-                #to avoid divergence
-                if(q1 == 0 and q2 == 0 and q3 == 0):
-                    k_vec[q] = (rec_vec[0] * q1/ kmesh[0]) + (rec_vec[1] * q2 / kmesh[1]) + (rec_vec[2] * q3 / kmesh[2]) + np.array([0.01, 0.0, 0.0])
-                else:
-                    k_vec[q] = (rec_vec[0] * q1/ kmesh[0]) + (rec_vec[1] * q2 / kmesh[1]) + (rec_vec[2] * q3 / kmesh[2])
-                
+    q = 0
+    for q1 in range(-int(0.5 * kmesh[0]),  int(0.5 * kmesh[0])):
+        for q2 in range(-int(0.5 * kmesh[1]),  int(0.5 * kmesh[1])):
+            # to avoid divergence
+            if(q1 == 0 and q2 == 0):
+                k_vec[q] = (rec_vec[0] * q1/ kmesh[0]) + (rec_vec[1] * q2 / kmesh[1]) + np.array([0.01, 0.0, 0.0])
+            else:
+                k_vec[q] = (rec_vec[0] * q1/ kmesh[0]) + (rec_vec[1] * q2 / kmesh[1])
+            q +=1
                 
                 
     print('... kmesh is ready')
@@ -36,8 +35,8 @@ def energy_contour_preparation(ncol, nrow, e_fermi, e_low, smearing):
     num_freq = ncol + 2 * nrow
     de = complex((e_fermi - e_low) / ncol, smearing / nrow)
 
-    E = np.zeros(num_freq, dtype=np.complex128)
-    dE = np.zeros(num_freq, dtype=np.complex128)
+    freq = np.zeros(num_freq, dtype=np.complex128)
+    d_freq = np.zeros(num_freq, dtype=np.complex128)
     e_const = complex(e_low, 0)
 
     idx_x = 0
@@ -56,11 +55,11 @@ def energy_contour_preparation(ncol, nrow, e_fermi, e_low, smearing):
             idx = 0
             e_const = complex(e_fermi, smearing)
         
-        E[i] = e_const + complex(idx * idx_x * (de).real, idx * idx_y * (de).imag)
-        dE[i] = complex(idx_x * (de).real, idx_y * (de).imag)
+        freq[i] = e_const + complex(idx * idx_x * (de).real, idx * idx_y * (de).imag)
+        d_freq[i] = complex(idx_x * (de).real, idx_y * (de).imag)
         idx = idx+1
 
-    return E, dE
+    return freq, d_freq
 
 
 
@@ -105,29 +104,26 @@ def calc_green(freq, k_vec, Ham_K, selfen, smearing):
     
     num_kpoints = k_vec.shape[0]
     num_freq = freq.shape[0]
-    weight = 1/num_kpoints
  
-    greenR = np.zeros((2, num_freq), dtype=np.complex128)
-    greenR_corr = np.zeros((2, num_freq), dtype=np.complex128)
+    greenR = np.zeros((2, num_kpoints, num_freq), dtype=np.complex128)
+    greenR_corr = np.zeros((2, num_kpoints, num_freq), dtype=np.complex128)
     
     for num in range(num_freq):
-        for  e in range(num_kpoints):
+        for e in range(num_kpoints):
             for z in range(2):
                 #G = 1/(E - H)
-                greenR[z, num] += 1/(freq[num] - Ham_K[e, z] + 1j*smearing)
-                greenR_corr[z, num] += 1/((freq[num] - Ham_K[e, z]) - selfen[z, e, num])
+                greenR[z, e, num] += 1/(freq[num] - Ham_K[e, z] + 1j*smearing)
+                greenR_corr[z, e, num] += 1/((freq[num] - Ham_K[e, z]) - selfen[z, e, num])
         
-        
-    greenR *= -(1/np.pi) * weight
-    greenR_corr *= -(1/np.pi) * weight
+    greenR *= -(1/np.pi) 
+    greenR_corr *= -(1/np.pi)
     
     return greenR, greenR_corr
-    
-    
-
 
         
-def cal_el_k(t, delta, e_fermi, cell_vec, k_vec): 
+    
+    
+def calc_electron(t, delta, e_fermi, cell_vec, k_vec): 
     
     num_kpoints = k_vec.shape[0]
     el_k = np.zeros((num_kpoints, 2), dtype=np.complex128)
@@ -159,19 +155,89 @@ def cal_el_k(t, delta, e_fermi, cell_vec, k_vec):
 
 
 
-def cal_ph_q(v, k_vec):
+#Model of phonons and elph coupling constant
+def calc_phonon(model, d_const, v, hw0, k_vec, electron_k, verbose):
     num_kpoints = k_vec.shape[0]
-    ph_q = np.zeros((num_kpoints, 2))
     
-    v = v * 0.658e-5 #(eV*s/m) 
+    hbar = 6.582e-16 # (eV * s)
+    m = 10 * 1.036e-28 # (amu -> eV * s^2/A^2) 
+    a = 5 # unit cell parameter A 
+    kB = 8.61733e-5 # eV/K
     
-    for q in range(num_kpoints):
-        q_abs = np.linalg.norm(k_vec[q])
-        ph_q[q, 0] = v * q_abs # (v * \hbar q) acoustic mode 
-        ph_q[q, 1] = v * q_abs # (v * \hbar q) acoustic mode 
-        
-    return ph_q 
+    if(model == 'D'):
+        v = v * 1e10 #(m/s - > A/s) 
+        if(verbose):
+            Tc = np.pi * hbar * v / (kB * a) 
+            print("Debye model of phonons is used")
+            print("Speed of sound (m/s):", v / 1e10)
+            print("Effective frequency (eV):", np.pi * hbar * v / a)
+            print("Deformation value (eV):", d_const)
+            print("Debye temperature (K):",Tc)
+                       
+        lambda_elph = calc_lambda(electron_k)
+        lambda_elph *= d_const**2 / (2 * m * v**2) 
+        ph_q = np.zeros((num_kpoints, 2))
+        g = np.zeros(num_kpoints)
+        for q in range(num_kpoints):
+            q_abs = np.linalg.norm(k_vec[q])  # 1/A
+            ph_q[q, 0] = hbar * v * q_abs # (eV * s) * (A/s) * (1/A) -> eV  
+            ph_q[q, 1] = hbar * v * q_abs 
+            
+            g[q] = np.sqrt( hbar**2 /(m * ph_q[q, 0])) * q_abs * d_const  # eV 
+                 
+                   
+    elif(model == 'E'):
+        q0 = np.pi / a  # 1/A
+        if(verbose):
+            Tc = hw0 / kB
+            print("Einstein model of phonons is used")
+            print("Phonon frequency (eV):", hw0)
+            print("Deformation value (eV):", d_const)
+            print("Einstein temperature (K):",Tc)
+    
+        lambda_elph = calc_lambda(electron_k)
+        lambda_elph *= (d_const * hbar * q0)**2 / (2 * m * hw0**2)  
+        ph_q = hw0 * np.ones((num_kpoints, 2))
+        g = np.zeros(num_kpoints)
+        for q in range(num_kpoints):
+            g[q] = np.sqrt( hbar**2 /(m * hw0)) * q0 * d_const # eV
+          
+    else:
+        print("Set the correct model!!!")
+        return None
+            
+    return ph_q, g, lambda_elph
 
+
+
+#calculate el-ph coupling constant
+@jit(nopython=True, parallel=True)
+def calc_lambda(el_k):
+    num_kpoints = el_k.shape[0]
+    weight = 1 / num_kpoints
+    el_k = el_k.reshape(kmesh[0], kmesh[1], 2).real
+    
+    def dirac_delta(kT,E):
+        if(np.abs(E/kT) < 20):
+            delta = (np.exp(E/kT)/kT)/(1 + np.exp(E/kT))**2
+        else:
+            delta = 0
+        return delta
+    
+    lambda_elph = 0.0
+    DOS_F = 0.0
+    for k1 in prange(kmesh[0]):
+        for k2 in prange(kmesh[1]):
+            DOS_F -= dirac_delta(el_k[k1, k2, 0], 0.01)
+            for q1 in range(kmesh[0]):
+                for q2 in range(kmesh[1]):
+                    kq1 = (k1 + q1) % kmesh[0]
+                    kq2 = (k2 + q2) % kmesh[1]
+                    lambda_elph += dirac_delta(el_k[k1, k2, 0], 0.01) * dirac_delta(el_k[kq1, kq2, 0], 0.01)
+
+    lambda_elph *= weight / DOS_F
+    return lambda_elph 
+    
 
 
 @jit(nopython=True, parallel=True)
@@ -181,7 +247,6 @@ def elph_selfen(g, kT, kmesh, freq, el_k, ph_q):
         return 1.0 / (np.exp(E / kT) - 1)
 
     def fermi(kT,E):
-#         return np.where(E.real < 0, 1.0, 0.0) + 0j  # step function
         return 1 / (np.exp(E / kT) + 1)
 
     num_freq = freq.shape[0]
@@ -194,6 +259,7 @@ def elph_selfen(g, kT, kmesh, freq, el_k, ph_q):
 
     el_k = el_k.reshape(kmesh[0], kmesh[1], 2)
     ph_q = ph_q.reshape(kmesh[0], kmesh[1], 2)
+    g = g.reshape(kmesh[0], kmesh[1])
 
     for k1 in prange(kmesh[0]):
         for k2 in prange(kmesh[1]):
@@ -208,13 +274,13 @@ def elph_selfen(g, kT, kmesh, freq, el_k, ph_q):
                         fermi_term = fermi(kT, el_k[kq1, kq2, z])
                         el_energy = el_k[kq1, kq2, z]
                         ph_energy = ph_q[q1, q2, z]
+                        g2_const = g[q1, q2]**2 
                     
                         for w in range(num_freq):
-                            selfen[z, k, w] += (bose_term + fermi_term) / (freq[w] - el_energy + ph_energy + 1j*delta) + \
-                            (bose_term + 1 - fermi_term) / (freq[w] - el_energy - ph_energy + 1j*delta) 
+                            selfen[z, k, w] += g2_const * ((bose_term + fermi_term) / (freq[w] - el_energy + ph_energy + 1j*delta) + \
+                            (bose_term + 1 - fermi_term) / (freq[w] - el_energy - ph_energy + 1j*delta)) 
 
-
-    selfen *= g * weight
+    selfen *= weight
     
     if(np.any(np.isnan(selfen))):
         print("There is a divergence!!!")
@@ -223,7 +289,7 @@ def elph_selfen(g, kT, kmesh, freq, el_k, ph_q):
 
 
 @jit(nopython=True)    
-def plot_dos(freq, energy_k):
+def plot_dos(freq, energy_k, kT):
     
     def dirac_delta(kT,E_tot):
         E = E_tot.real
@@ -236,60 +302,58 @@ def plot_dos(freq, energy_k):
     num_kpoints = energy_k.shape[0]
     num_enpoints = freq.shape[0]
     dos_F = np.zeros((1000, 2))
-    kT = 0.01
     
     for spin in range(2):
         for en in range(1000):
             for k in range(num_kpoints):        
                 dos_F[en, spin] += dirac_delta(kT, freq[en] - energy_k[k, spin])
                 
-        dos_F[en, spin] = dos_F[en, spin]/num_kpoints 
-        
-    return dos_F
+    return dos_F/num_kpoints
 
 #############################################################################
 
 t = -0.1 # hopping (in eV)
 delta = 0.4 # spin splitting (in eV)
 e_fermi = 0 # Fermi energy
-smearing = 0.001 # smearing for integration
+smearing = 1e-3 # smearing for integration
 
-ncol = 600
-nrow = 300
+ncol = 500
+nrow = 10
 num_freq = ncol + 2 * nrow 
 
-kmesh = np.array([50, 50, 1])
+kmesh = np.array([60, 60, 1])
 num_kpoints = np.prod(kmesh)
 print('kmesh: ', kmesh)
 
-cell_vec = np.array([[1.0,  0.0, 0.0],
-                     [0.0,  1.0, 0.0],
-                     [0.0,  0.0, 10.0]])
+cell_vec = np.array([[5.0,  0.0, 0.0],
+                     [0.0,  5.0, 0.0],
+                     [0.0,  0.0, 20.0]])
 
 
 
 k_vec = kmesh_preparation(kmesh, cell_vec)
-el_k = cal_el_k(t, delta, e_fermi, cell_vec, k_vec)
+el_k = calc_electron(t, delta, e_fermi, cell_vec, k_vec)
 
 e_low = np.min(el_k.real) - 0.2*delta # min energy for integration
 
+#plot 2d map
 freq = np.linspace(e_low, e_fermi, ncol)
 index_temp = np.array([1, 0])
 E, dE = energy_contour_preparation(ncol, nrow, e_fermi, e_low, smearing)
 
-
-v = np.linspace(300, 3000, 50) # sound velocity (in m/c)
+v = np.linspace(1000, 3000, 50) # sound velocity (in m/c)
 T = np.linspace(10, 300, 50)
 data = np.zeros((50,50), dtype=np.float64)
 
 for i in range(50):
     for j in range(50):
-        print(i,j)
-        ph_q = cal_ph_q(v[i], k_vec)
-        selfen = elph_selfen(0.01, T[j]*0.00008617, kmesh, freq, el_k, ph_q)
+        d  = 0.05 * v[i] /410 
+        ph_q, g_q, lambda_elph = calc_phonon('D', d, v[i], 0.01, k_vec, el_k, False)
+        selfen = elph_selfen(g_q, T[j]*0.00008617, kmesh, freq, el_k, ph_q)
         selfen = np.pad(selfen, [(0, 0), (0, 0),(nrow, nrow)])
         J = calc_exchange(index_temp, num_freq, cell_vec, k_vec, E, dE, el_k, selfen)
         data[i, j] = J
+        print(v[i], j, J, lambda_elph)
     
 
 
@@ -303,9 +367,9 @@ ax = fig.add_subplot(111)
 ax.pcolor(xmesh, ymesh, data, shading='nearest', cmap='Blues')
 ax.set_xlabel('T(K)')
 ax.set_ylabel('v(m/c)')
-fig.savefig(os.path.join(results_dir, 'phase_diagram.png'), dpi=400, facecolor='w', transparent=False, bbox_inches='tight')
+fig.savefig(os.path.join(results_dir, '1.png'), dpi=400, facecolor='w', transparent=False, bbox_inches='tight')
 
 
 with open(os.path.join(results_dir, 'J_plot.dat'), "w") as fp:
-    for i in range(10):
+    for i in range(50):
         print('{0.real:.4f}'.format(T[i]),'  '.join('{0.real:.4f}'.format(item) for item in data[:,i]), file=fp)
